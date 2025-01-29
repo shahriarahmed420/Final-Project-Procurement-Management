6db099a1c152189c0aa63871cdd7d58c491d58b1
Shahriar Ahmed <shahriar.ahmed@bjitacademy.com>
2025-01-29 17:40:41 +0600

    Project Planning and Initial Module Configuration

---
+++ procurement_management/controllers/controllers.py
@@ -0,0 +1,6 @@
+# from odoo.addons.portal.controllers.portal import CustomerPortal, pager as portal_pager
+# from odoo.http import request
+# from odoo import http, _
+
+from odoo import http
+from odoo.http import request
---
+++ procurement_management/models/otp_model.py
@@ -0,0 +1,31 @@
+from email.policy import default
+
+from odoo import models, fields, api
+import random
+import string
+from datetime import datetime, timedelta
+
+class SupplierOTP(models.Model):
+    _name = 'supplier.otp'
+    _description = 'Supplier OTP Verification'
+
+    email = fields.Char(required=True, index=True)
+    otp = fields.Char(required=True)
+    expiration_time = fields.Datetime(required=True)
+    is_verified = fields.Boolean(default=False)
+
+    @api.model
+    def generate_otp(self, email):
+        self.env['supplier.otp'].search([('email', '=', email)]).unlink()
+        otp_code = ''.join(random.choices(string.digits, k=6))
+        expiration = datetime.now() + timedelta(minutes=5)
+        otp_record = self.create({'email': email, 'otp': otp_code, 'expiration_time': expiration})
+        return otp_record
+
+    def validate_otp(self, email, otp):
+        otp_record = self.search([('email', '=', email), ('otp', '=', otp), ('expiration_time', '>', datetime.now())], limit=1)
+
+        if otp_record:
+            otp_record.write({'is_verified': True})
+            return True
+        return False
